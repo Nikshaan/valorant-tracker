3049efd1c4e7cf97f0f9cc231ef7b0134049d8c3
Nikshaan <shettynikshaan@gmail.com>
2025-04-18 00:13:41 +0530

    update: packages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import logo from "../assets/logo-valo.png";
+import yoru from "../assets/yoru.jpg";
 
 const apiKey = import.meta.env.VITE_API_KEY;
 
@@ -34,16 +35,17 @@ const Home : React.FC = () => {
   return (
     <div className="text-white h-[100svh] bg-[#C3C7CB] relative flex flex-col items-center pb-16">
       <Navbar />
-      <div style={{ backgroundImage: "url('src/assets/yoru.jpg')" }} className="m-auto border-4 border-t-[#848584] border-l-[#848584] border-b-white border-r-white bg-cover bg-center flex flex-col justify-center items-center gap-5 bg-no-repeat w-[80%] h-[50%]">
-        <div className="flex flex-col bg-[#C3C7CB] p-10 px-20 justify-center items-center gap-5 border-4 border-b-black border-r-black border-t-white border-l-white">
-        <img src={logo} className="h-20"/>
-        <div className="bg-[#C3C7CB] text-black border-4 border-t-[#848584] border-l-[#848584] border-b-white border-r-white flex flex-col">
-          <form onSubmit={handleSubmit}>
-              <input type="text"  value={userInput} onChange={(e) => {setUserInput(e.target.value)}} className="py-1 px-2 placeholder-black border-2 w-fit" placeholder="Enter player name#tag : "/>
-              <button type="submit" className="bg-[#C3C7CB] px-4 py-1 border-2 border-b-black border-r-black border-t-white border-l-white text-black cursor-pointer">Search</button>
-          </form>
-          <p className={`text-center text-red-800 ${error?"flex justify-center items-center":"hidden"}`}>{error}</p>
-        </div>
+      <div className="m-auto relative overflow-hidden border-4 border-t-[#848584] border-l-[#848584] border-b-white border-r-white bg-cover bg-center flex flex-col justify-center items-center gap-5 bg-no-repeat w-[80%] h-[50%]">
+        <img src={yoru} />
+        <div className="flex absolute flex-col bg-[#C3C7CB] p-10 px-20 justify-center items-center gap-5 border-4 border-b-black border-r-black border-t-white border-l-white">
+          <img src={logo} className="h-20"/>
+          <div className="bg-[#C3C7CB] text-black border-4 border-t-[#848584] border-l-[#848584] border-b-white border-r-white flex flex-col">
+            <form onSubmit={handleSubmit}>
+                <input type="text"  value={userInput} onChange={(e) => {setUserInput(e.target.value)}} className="py-1 px-2 placeholder-black border-2 w-fit" placeholder="Enter player name#tag : "/>
+                <button type="submit" className="bg-[#C3C7CB] px-4 py-1 border-2 border-b-black border-r-black border-t-white border-l-white text-black cursor-pointer">Search</button>
+            </form>
+            <p className={`text-center text-red-800 ${error?"flex justify-center items-center":"hidden"}`}>{error}</p>
+          </div>
         </div>
       </div>
     </div>
