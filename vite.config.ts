b987bc2e2b414809d95ce426fff51e79d0373179
Nikshaan <shettynikshaan@gmail.com>
2025-04-17 23:11:09 +0530

    update: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-  base: "/"
+  base: "/",
+  resolve: {
+    alias: {
+      crypto: 'crypto-js'
+    }
+  },
+  define: {
+    'process.env.NODE_DEBUG': 'false',
+    'global': {}
+  }
 })
